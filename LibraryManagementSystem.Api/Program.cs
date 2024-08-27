9fef9537d71e03e5b2fe5d1690ff8ee465112ff7
shilpb20 <shilpb20@gmail.com>
2024-08-27 14:08:23 +1000

    Coonfigured Automapper service.

--- LibraryManagementSystem.Api/Program.cs
+++ LibraryManagementSystem.Api/Program.cs
@@ -17,6 +17,7 @@ builder.Host.UseSerilog();
 builder.Services.AddDbContext<ApplicationDbContext>(
         options => options.UseSqlServer(
             builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
