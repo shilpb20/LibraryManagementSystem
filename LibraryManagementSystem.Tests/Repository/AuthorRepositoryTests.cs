7ed14a9a543cdc0c27fec0e3ad3ad1c7b10de3db
shilpb20 <shilpb20@gmail.com>
2024-08-28 18:21:56 +1000

    AuthorRepository - Add Author.
    - Added tests for Add method in AuthorReposittory.
    - Implemneted AddAsync method in AuthorRepository.

---
+++ LibraryManagementSystem.Tests/Repository/AuthorRepositoryTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using LibraryManagementSystem.Core.Models;
+using LibraryManagementSystem.Infrastructure;
+using LibraryManagementSystem.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Xunit.Abstractions;
+
+namespace LibraryManagementSystem.Tests.Repository
+{
+    public class AuthorRepositoryTests : TestBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly AuthorRepository _authorRepository;
+
+        public AuthorRepositoryTests(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "LibraryManagementSystem_Api_Test")
+                .Options;
+
+            _context = new ApplicationDbContext(dbContextOptions);
+            _authorRepository = new AuthorRepository(_context);
+        }
+
+        [Fact]
+        public async Task AddAuthor_ValidObject_AddsSuccessfully()
+        {
+            //Arrange
+            //Act
+            var author = new Author()
+            {
+                Id = 1,
+                Name = "Simon Sinek"
+            };
+
+            await _authorRepository.AddAsync(author);
+
+            //Assert
+            Author addedObject = await _context.Authors.FirstOrDefaultAsync(x => x.Name == author.Name);
+
+            addedObject.Should().NotBeNull();
+            addedObject?.Id.Should().Be(author.Id);
+            addedObject?.Name.Should().Be(author.Name);
+
+        }
+    }
+}
